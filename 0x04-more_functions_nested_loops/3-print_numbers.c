ee60319b4d1fe95c4cc2b0d53751c1412f57b00c
bmuraya <babumuraya@gmail.com>
2023-02-23 05:09:48 -0800

    task 2 and 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int number = 9;
+for (number = 9; number < 12; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
