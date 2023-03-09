63b722c471cba996610b38e3526dc892db986100
bmuraya <babumuraya@gmail.com>
2023-03-09 11:39:45 -0800

    change on 5,6,100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,45 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
 /**
-* is_palindrome - returns true if the given string is a palindrome
-*@s: string to check
-*Return: true if the given string is a palindrome
-*/
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 if it is, 0 it's not
+ */
 
 int is_palindrome(char *s)
 {
-	int index = 0;
-	int len = find_strlen(s);
-
-	if (!(*s))
-		return (1);
-
-	return (check_palindrome(s, len, index));
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
 /**
- * find_strlen - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ * Return: length of the string
  */
-int find_strlen(char *s)
+int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*(s + len))
-	{
-		len++;
-		len += find_strlen(s + len);
-	}
-
-	return (len);
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * check_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- * @len: The length of s.
- * @index: The index of the string to be checked.
- *
- * Return: If the string is a palindrome - 1.
- *         If the string is not a palindrome - 0.
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ * Return: 1 if palindrome, 0 if not
  */
-int check_palindrome(char *s, int len, int index)
+int check_pal(char *s, int i, int len)
 {
-	if (s[index] == s[len / 2])
-		return (1);
-
-	if (s[index] == s[len - index - 1])
-		return (check_palindrome(s, len, index + 1));
-
-	return (0);
+if (*(s + i) != *(s + len - 1))
+return (0);
+if (i >= len)
+return (1);
+return (check_pal(s, i + 1, len - 1));
 }
