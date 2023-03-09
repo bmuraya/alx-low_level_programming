d1992ef29aa53fb699ff70bbdb64bbc27b181273
bmuraya <babumuraya@gmail.com>
2023-03-09 11:30:29 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,14 +6,12 @@
 *
 * Return: the square root of n
 */
-
 int _sqrt_recursion(int n)
 {
 if (n == 0 || n == 1)
 return (n);
 return (_sqrt(0, n));
 }
-
 /**
  * _sqrt - returns the square root of a number
  * @n: test number
@@ -21,7 +19,6 @@ return (_sqrt(0, n));
  *
  * Return: the square root of n
  */
-
 int _sqrt(int n, int x)
 {
 if (n > x / 2)
