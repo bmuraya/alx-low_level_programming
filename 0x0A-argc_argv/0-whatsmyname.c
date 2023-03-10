424d643081141b2005a9ac3838960679f687d182
bmuraya <babumuraya@gmail.com>
2023-03-10 10:47:24 -0800

    task one

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,7 @@
 *f you rename the program, it will print the new name, without having to compile it again
 *ou should not remove the path before the name of the program
 */
-
-int main(int argc, char *argv[])
-{
+int main(int argc __attribute__((unused)), char *argv[])
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
