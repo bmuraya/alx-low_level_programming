b0659878f581f9f59b77c43fb11dce60f73653fd
bmuraya <babumuraya@gmail.com>
2023-04-04 11:48:58 -0700

    linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,36 +1,27 @@
 #include "lists.h"
 
 /**
- *add_nodeint_end - adds a node to the end of a linked list
- *@head: pointer to the head of the list
- *@n: number to be used as content
+ *add_nodeint - Adds a new node at the beginning
+ *of a listint_t list
+ *@head: A pointer to the address of the head of the listint_t list
+ *@n: the integer for the new node to contain
  *
- *
- *Return: address of the newly added node
+ *Return: NULL if function fails
+ *Or address of element
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	listint_t *cursor = *head;
+	listint_t *new;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = NULL;
-	}
-	else
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-	if (cursor != NULL)
-	{
-		while (cursor->next != NULL)
-			cursor = cursor->next;
 
-		cursor->next = new_node;
-	}
-	else
-		*head = new_node;
-	return (new_node);
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
-
