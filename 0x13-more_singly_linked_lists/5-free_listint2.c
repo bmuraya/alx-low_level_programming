3f3e4f36cddab7ee4c2a7e5b5ab228e41cbe20f4
bmuraya <babumuraya@gmail.com>
2023-04-06 15:30:44 +0300

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- File: 5-free_listint2.c
- Auth Babu Muraya
-
-*/
-
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
 void free_listint2(listint_t **head)
 {
-listint_t *tmp;
-if (head == NULL)
-return;
-while (*head)
-{
-tmp = (*head)->next;
-free(*head);
-*head = tmp;
-}
-head = NULL;
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
 }
