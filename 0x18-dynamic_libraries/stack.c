d8d7041e8dc5d2adf867d0777da4d246c4f0811e
bmuraya <babumuraya@gmail.com>
2023-07-08 15:54:28 +0300

    create table task one

---
+++ 0x18-dynamic_libraries/stack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define N 5
+
+int stack[N];
+int top = -1;
+
+void main()
+{
+    int ch;
+    clear();
+do
+{
+    printf("enter choice")
+}    
+}
--- 0x1A-hash_tables/0-main.c
+++ 0x1A-hash_tables/0-main.c
@@ -4,14 +4,14 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for
+ * main-check the code for
  *
- * Return: Always EXIT_SUCCESS.
+ * Return:Always EXIT_SUCCESS.
  */
 int main(void)
 {
-hash_table_t *ht;
-ht = hash_table_create(1024);
-printf("%p\n", (void *)ht);
-return (EXIT_SUCCESS);
+    hash_table_t *ht;
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
 }
